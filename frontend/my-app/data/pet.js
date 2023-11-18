5fa1c090b308fa850a972990a1df9bc024ac253c
Carolina Fernandes <mcs.fernandes@campus.fct.unl.pt>
2023-11-18 21:11:11 +0000

    File with pets data created

---
+++ frontend/my-app/data/pet.js
@@ -0,0 +1,18 @@
+export const petsData = [
+    /* Gadelha will update :) */
+    {
+      id: 1,
+      name: 'Fluffy',
+      breed: 'Golden Retriever',
+      age: 3,
+    },
+    {
+      id: 2,
+      name: 'Whiskers',
+      breed: 'Maine Coon',
+      age: 5,
+    },
+    // Add more pet objects as needed
+  ];
+  
+  
