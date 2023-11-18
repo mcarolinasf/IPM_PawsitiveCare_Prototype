39111682435ec83abed6cbae045121be72a1fead
Carolina Fernandes <mcs.fernandes@campus.fct.unl.pt>
2023-11-18 23:11:58 +0000

    Pet ids added

--- frontend/my-app/data/userData.js
+++ frontend/my-app/data/userData.js
@@ -4,6 +4,6 @@ export default userData = {
         pwd:'admin',
         name: 'Administrator',
         photoUrl: 'https://images.healthshots.com/healthshots/en/uploads/2020/12/08182549/positive-person.jpg',
-        pets: [{}]
+        petIds: [0,1,2]
     }
 }
