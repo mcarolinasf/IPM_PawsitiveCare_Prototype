5d4cf91a7203052a0d4d6239bdadddf79b671135
Carolina Fernandes <mcs.fernandes@campus.fct.unl.pt>
2023-11-21 15:32:55 +0000

    Simplifiying

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
 
 const express = require('express');
 const app = express();
-const mongooseConnection = require('./config/connection'); // Import Mongoose connection
 
-const User = require('./models/user'); // Import your Mongoose model
+
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
-// Start the server once the MongoDB connection is established
-mongooseConnection.once('open', () => {
-  const server = app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
-  });
 });
 
-// Use your routes after the server starts
-const userRoutes = require('./routes/user');
-app.use('/api', userRoutes);
 
 
+
+
+app.get("/", (req, res) => {
+    
+    res.status(200).json("Hey there");
+
+});
