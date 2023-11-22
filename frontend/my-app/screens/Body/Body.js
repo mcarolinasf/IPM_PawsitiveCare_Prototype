c693e403eb4bf1b225807020c8eee2c92df8a368
Rodrigo <rm.mesquita@campus.fct.unl.pt>
2023-11-22 05:54:49 +0000

    scrolls fixed

--- frontend/my-app/screens/Body/Body.js
+++ frontend/my-app/screens/Body/Body.js
@@ -1,87 +1,139 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
-import Header from '../../components/Header/Header'
-import TextInputDefault from '../../components/TextInputDefault/TextInputDefault'
-import { globalStyles } from '../../styles/globalStyles'
-import { BodySTyles as BodyStyles } from './BodyStyles'
+import React, { useState, useContext, useEffect } from "react";
+import { View, Text, SafeAreaView, ScrollView, Image } from "react-native";
+import Header from "../../components/Header/Header";
+import TextInputDefault from "../../components/TextInputDefault/TextInputDefault";
+import { globalStyles } from "../../styles/globalStyles";
+import { BodySTyles as BodyStyles } from "./BodyStyles";
 import chart from "../../assets/ChartWeight.png";
-import { PetStyles } from '../Pet/PetStyles'
-
-
+import { PetStyles } from "../Pet/PetStyles";
 
 export const Body = ({ navigation, route }) => {
+  const { pet } = route.params;
 
-    const { pet } = route.params;
+  const [editedPet, setEditedPet] = useState({
+    name: "",
+    age: "",
+    gender: "",
+    breed: "",
+    color: "",
+    typeOfCoat: "",
+    tail: "",
+    distinguishMarks: "",
+    height: "",
+    weight: "",
+    photoUrl: "",
+  });
 
-    const [editedPet, setEditedPet] = useState({
-        name: '',
-        age: '',
-        gender: '',
-        breed: '',
-        color: '',
-        typeOfCoat: '',
-        tail: '',
-        distinguishMarks: '',
-        height: '',
-        weight: '',
-        photoUrl: ''
+  return (
+    <SafeAreaView style={globalStyles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header title={"Body"} goBack showProfile />
+        <View style={BodyStyles.container} {...globalStyles.shadow}>
+          <Image
+            style={BodyStyles.image}
+            source={{
+              uri: pet.photoUrl,
+            }}
+          />
+        </View>
+        <View style={{ paddingHorizontal: 10 }}>
+          <TextInputDefault
+            label={"Name"}
+            setFunction={(value) => setEditedPet({ ...editedPet, name: value })}
+            value={editedPet.name}
+            placeholder={pet.name}
+          />
 
-    })
+          <View style={BodyStyles.row}>
+            <View style={{ flex: 1, paddingRight: 10 }}>
+              <TextInputDefault
+                label={"Age"}
+                setFunction={(value) =>
+                  setEditedPet({ ...editedPet, age: value })
+                }
+                value={editedPet.age}
+                placeholder={pet.age.toString()}
+              />
+              <TextInputDefault
+                label={"Breed"}
+                setFunction={(value) =>
+                  setEditedPet({ ...editedPet, breed: value })
+                }
+                value={editedPet.breed}
+                placeholder={pet.breed}
+              />
+              <TextInputDefault
+                label={"Type of coat"}
+                setFunction={(value) =>
+                  setEditedPet({ ...editedPet, typeOfCoat: value })
+                }
+                value={editedPet.typeOfCoat}
+                placeholder={pet.typeOfCoat}
+              />
+            </View>
 
-    return (
-        <SafeAreaView style={globalStyles.container}>
-            <ScrollView>
-                <Header title={'Body'} goBack showProfile />
-                <View style={BodyStyles.container}{...globalStyles.shadow} >
-                    <Image
-                        style={BodyStyles.image}
-                        source={{
-                            uri: pet.photoUrl
-                        }}
-                    />
-                </View>
-                <View style={{ paddingHorizontal: 10 }}>
-                    <TextInputDefault label={'Name'} setFunction={(value) => setEditedPet({ ...editedPet, name: value })} value={editedPet.name} placeholder={pet.name} />
-
-                    <View style={BodyStyles.row}>
-                        <View style={{ flex: 1, paddingRight: 10 }}>
-                            <TextInputDefault label={'Age'} setFunction={(value) => setEditedPet({ ...editedPet, age: value })} value={editedPet.age} placeholder={pet.age.toString()} />
-                            <TextInputDefault label={'Breed'} setFunction={(value) => setEditedPet({ ...editedPet, breed: value })} value={editedPet.breed} placeholder={pet.breed} />
-                            <TextInputDefault label={'Type of coat'} setFunction={(value) => setEditedPet({ ...editedPet, typeOfCoat: value })} value={editedPet.typeOfCoat} placeholder={pet.typeOfCoat} />
-                        </View>
-
-                        <View style={{ flex: 1 }}>
-                            <TextInputDefault label={'Gender'} setFunction={(value) => setEditedPet({ ...editedPet, gender: value })} value={editedPet.gender} placeholder={pet.gender} />
-                            <TextInputDefault label={'Color'} setFunction={(value) => setEditedPet({ ...editedPet, color: value })} value={editedPet.color} placeholder={pet.color} />
-                            <TextInputDefault label={'Tail'} setFunction={(value) => setEditedPet({ ...editedPet, tail: value })} value={editedPet.tail} placeholder={pet.tail} />
-                        </View>
-                    </View>
-                    <TextInputDefault label={'Distinguish Marks'} setFunction={(value) => setEditedPet({ ...editedPet, distinguishMarks: value })} value={editedPet.distinguishMarks} placeholder={pet.distinguishMarks} />
-                    <View style={BodyStyles.row}>
-                        <View style={{ flex: 1, paddingRight: 10 }}>
-                            <TextInputDefault label={'Weight'} setFunction={(value) => setEditedPet({ ...editedPet, weight: value })} value={editedPet.weight} placeholder={pet.weight} />
-                        </View>
-                        <View style={{ flex: 1 }}>
-                            <TextInputDefault label={'Height'} setFunction={(value) => setEditedPet({ ...editedPet, height: value })} value={editedPet.height} placeholder={pet.height} />
-                        </View>
-
-                    </View>
-                </View>
-                <View style={{ ...PetStyles.container, ...globalStyles.shadow }}>
-                    <Image
-                        style={PetStyles.chart}
-                        source={chart}
-                    />
-
-
-                </View>
-
-
-
-            </ScrollView>
-
-        </SafeAreaView>
-    )
-
-
-}
+            <View style={{ flex: 1 }}>
+              <TextInputDefault
+                label={"Gender"}
+                setFunction={(value) =>
+                  setEditedPet({ ...editedPet, gender: value })
+                }
+                value={editedPet.gender}
+                placeholder={pet.gender}
+              />
+              <TextInputDefault
+                label={"Color"}
+                setFunction={(value) =>
+                  setEditedPet({ ...editedPet, color: value })
+                }
+                value={editedPet.color}
+                placeholder={pet.color}
+              />
+              <TextInputDefault
+                label={"Tail"}
+                setFunction={(value) =>
+                  setEditedPet({ ...editedPet, tail: value })
+                }
+                value={editedPet.tail}
+                placeholder={pet.tail}
+              />
+            </View>
+          </View>
+          <TextInputDefault
+            label={"Distinguish Marks"}
+            setFunction={(value) =>
+              setEditedPet({ ...editedPet, distinguishMarks: value })
+            }
+            value={editedPet.distinguishMarks}
+            placeholder={pet.distinguishMarks}
+          />
+          <View style={BodyStyles.row}>
+            <View style={{ flex: 1, paddingRight: 10 }}>
+              <TextInputDefault
+                label={"Weight"}
+                setFunction={(value) =>
+                  setEditedPet({ ...editedPet, weight: value })
+                }
+                value={editedPet.weight}
+                placeholder={pet.weight}
+              />
+            </View>
+            <View style={{ flex: 1 }}>
+              <TextInputDefault
+                label={"Height"}
+                setFunction={(value) =>
+                  setEditedPet({ ...editedPet, height: value })
+                }
+                value={editedPet.height}
+                placeholder={pet.height}
+              />
+            </View>
+          </View>
+        </View>
+        <View style={{ ...PetStyles.container, ...globalStyles.shadow }}>
+          <Image style={PetStyles.chart} source={chart} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
--- frontend/my-app/screens/Pet/Pet.js
+++ frontend/my-app/screens/Pet/Pet.js
@@ -1,87 +1,107 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { View, Text, SafeAreaView, ScrollView, TouchableOpacity, Image, FlatList } from 'react-native'
-import { globalStyles } from '../../styles/globalStyles'
-import { PetStyles } from './PetStyles'
-import MenuCard from '../../components/MenuCard/MenuCard.js';
-import Header from '../../components/Header/Header.js';
-import { FontAwesome5 } from '@expo/vector-icons';
-import colors from '../../styles/colors';
-import TaskItem from '../../components/TaskItem/TaskItem.js';
-import navigationPaths from '../../navigation/navigationPaths';
-import UserSessionContext from '../../services/UserSessionContext.js';
-import { TasksData } from '../../data/TasksData.js';
+import React, { useContext, useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { globalStyles } from "../../styles/globalStyles";
+import { PetStyles } from "./PetStyles";
+import MenuCard from "../../components/MenuCard/MenuCard.js";
+import Header from "../../components/Header/Header.js";
+import { FontAwesome5 } from "@expo/vector-icons";
+import colors from "../../styles/colors";
+import TaskItem from "../../components/TaskItem/TaskItem.js";
+import navigationPaths from "../../navigation/navigationPaths";
+import UserSessionContext from "../../services/UserSessionContext.js";
+import { TasksData } from "../../data/TasksData.js";
 import chart from "../../assets/Chart.png";
-import { petsApi } from '../../api';
-
-
+import { petsApi } from "../../api";
 
 export const Pet = ({ navigation, route }) => {
+  const { pet } = route.params;
 
-    const { pet } = route.params;
+  const [tasks, setTasks] = useState([]);
 
-    const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    getData();
+  }, [pet]);
 
-    useEffect(() => {
-        getData()
-    }, [pet])
-
-
-    const getData = async () => {
-
-        try {
-            console.log(pet.idP)
-            const tasksRes = await petsApi.getPetTasks(pet.idP)
-            setTasks(tasksRes)
-        } catch (error) {
-            console.log("Error Message: " + error.message)
-        }
-
-
+  const getData = async () => {
+    try {
+      console.log(pet.idP);
+      const tasksRes = await petsApi.getPetTasks(pet.idP);
+      setTasks(tasksRes);
+    } catch (error) {
+      console.log("Error Message: " + error.message);
     }
+  };
 
-    const handleTaskPress = (key) => {
-        setTasks((prevTasks) => (
-            prevTasks.filter(task => task.id != key)
-        ));
-    }
+  const handleTaskPress = (key) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id != key));
+  };
 
-    return (
-        <SafeAreaView style={globalStyles.swipe}>
-            <ScrollView>
-                <Header title={pet.name} goBack showProfile />
-                <View style={PetStyles.container}{...globalStyles.shadow} >
-                    <Image
-                        style={PetStyles.image}
-                        source={{
-                            uri: pet.photoUrl
-                        }}
-                    />
-                </View>
-                <ScrollView horizontal={true}>
-                    <MenuCard iconName={'paw'} title={'Vet Ap.'} navigation={navigation} navigateTo={navigationPaths.vetAppointments} />
-                    <MenuCard iconName={'dog'} title={'Body'} navigation={navigation} navigateTo={navigationPaths.body} pet={pet} />
-                    <MenuCard iconName={'pills'} title={'Medication'} navigation={navigation} navigateTo={navigationPaths.medication} pet={pet} />
-                    <MenuCard iconName={'bone'} title={'Feeding'} navigation={navigation} navigateTo={navigationPaths.feeding} pet={pet} />
-                </ScrollView>
-                <Text style={globalStyles.subtitleText}>Today</Text>
-                <View>
-                    {
-                        tasks && tasks.filter(task => task.petId == pet.idP).map(task => (
-                            <TaskItem key={task.key} task={task} pressHandler={handleTaskPress} />
-                        ))
-                    }
-                </View>
-                <View style={{ ...PetStyles.container, ...globalStyles.shadow }}>
-                    <Image
-                        style={PetStyles.chart}
-                        source={chart}
-                    />
-
-
-                </View>
-
-            </ScrollView>
-        </SafeAreaView >
-    );
-
-}
+  return (
+    <SafeAreaView style={globalStyles.swipe}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header title={pet.name} goBack showProfile />
+        <View style={PetStyles.container} {...globalStyles.shadow}>
+          <Image
+            style={PetStyles.image}
+            source={{
+              uri: pet.photoUrl,
+            }}
+          />
+        </View>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <MenuCard
+            iconName={"paw"}
+            title={"Vet Ap."}
+            navigation={navigation}
+            navigateTo={navigationPaths.vetAppointments}
+          />
+          <MenuCard
+            iconName={"dog"}
+            title={"Body"}
+            navigation={navigation}
+            navigateTo={navigationPaths.body}
+            pet={pet}
+          />
+          <MenuCard
+            iconName={"pills"}
+            title={"Medication"}
+            navigation={navigation}
+            navigateTo={navigationPaths.medication}
+            pet={pet}
+          />
+          <MenuCard
+            iconName={"bone"}
+            title={"Feeding"}
+            navigation={navigation}
+            navigateTo={navigationPaths.feeding}
+            pet={pet}
+          />
+        </ScrollView>
+        <Text style={globalStyles.subtitleText}>Today</Text>
+        <View>
+          {tasks &&
+            tasks
+              .filter((task) => task.petId == pet.idP)
+              .map((task) => (
+                <TaskItem
+                  key={task.key}
+                  task={task}
+                  pressHandler={handleTaskPress}
+                />
+              ))}
+        </View>
+        <View style={{ ...PetStyles.container, ...globalStyles.shadow }}>
+          <Image style={PetStyles.chart} source={chart} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
